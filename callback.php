0f45bbea7517b8ad5d7075d36fb135256ea31682
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2021-01-12 21:41:17 +0700

    Last commit

--- callback.php
+++
@@ -1,5 +0,0 @@
-<?php
-$oauthToken = $_GET['oauth_token'];
-$oauthVerifier = $_GET['oauth_verifier'];
-file_put_contents("oauth_token.txt", $oauthToken);
-file_put_contents("oauth_verifier.txt", $oauthVerifier);
--- test.php
+++
@@ -1,2 +0,0 @@
-<?php
-echo "This is PHP test page.";
